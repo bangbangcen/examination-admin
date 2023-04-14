8bae9e19f6fdb5a7eb0b749ab7340051f13f7e7d
Tintin-PLUS <qinghuatintin@163.com>
2023-04-14 22:58:33 +0800

    体检订单页面V1.3
    状态切换为“体检中”时，关联assignment表的插入，获得竞争排队的能力
    后续等排队算法添加后更新

--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -76,7 +76,8 @@ const columns = [
       switch (params.row.status){
         case 0:return '未体检';break;
         case 1:return '体检中';break;
-        case 2:return '已完成';break;
+        case 2:return '增检中';break;
+        case 3:return '已完成';break;
       }
     },
     filters: [
@@ -89,11 +90,19 @@ const columns = [
           value: '1'
       },
       {
-          label: '已完成',
+          label: '增检中',
           value: '2'
+      },
+      {
+          label: '已完成',
+          value: '3'
       }
     ],
     filterMethod (value:any, row:any) {return row.status==value;}
+  },
+  {
+    title: "修改状态",
+    slot: "operation1",
   },
   {
     title: "增加检查",
@@ -107,6 +116,10 @@ const page = reactive({
     dname:"",
     curPage:1,
     pageSize:20,
+    modal:false,
+    modal2:false,
+    disabled:[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+    statusDisabled:true,
   }
 });
 
@@ -119,8 +132,7 @@ const state = reactive({
     data:res.data,
     order_id:"",
     name:"",
-    modal:false,
-    disabled:[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+    order_status:-1,
   }
 });
 
@@ -136,62 +148,113 @@ async function change_data(){
   },300)
 }
 
-async function openModal(id:string) {
-  state.info.order_id=id;
-  state.info.disabled=[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
-  const res=(await $axios.post("examination_order/assignment",state.info)).data;
-  for(var i=0;i<res.length;++i){
-    state.info.disabled[res[i].id]=true;
+async function openModal(id:string,status:number) {
+  if(status==0){
+    alert("该用户未开始体检");
   }
-  state.info.name="";
-  state.info.modal = true;
+  else{
+    state.info.order_id=id;
+    page.params.disabled=[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+    const res=(await $axios.post("examination_order/assignment",state.info)).data;
+    for(var i=0;i<res.length;++i){
+      page.params.disabled[res[i].id]=true;
+    }
+    state.info.name="";
+    page.params.modal = true;
+  }
+}
+
+async function openModal2(id:string,status:number) {
+  if(status==0){page.params.statusDisabled=false;}
+  state.info.order_id=id;
+  state.info.order_status=-1;
+  page.params.modal2 = true;
 }
 
 async function addCategory() {
   if(state.info.name==""){alert("选项为空");}
   else{
-    await $axios.post("examination_order/addCategory",state.info);
+    await $axios.post("examination_order/addAssignment",state.info);
     await proxy.$Message.success({ content: "增检成功" });
+  }
+}
+
+async function changeStatus() {
+  if(state.info.order_status==-1){alert("选项为空");}
+  else{
+    await $axios.post("examination_order/changeStatus",state.info);
+    await proxy.$Message.success({ content: "更改成功" });
+    res = await $axios.post("examination_order/list",page.params);
+    state.info.data=res.data;
+    if(state.info.order_status!=0){
+      await $axios.post("examination_order/newAssignment",state.info);
+    }
   }
 }
 </script>
 
 <template>
   <!-- 搜索框 -->
-  <Space direction="vertical" size="large" type="flex">
+  <!-- <Space direction="vertical" size="large" type="flex">
         <Input search enter-button v-model="page.params.dname" @on-change="change_data()" placeholder="输入体检人姓名或联系电话以搜索" />
-  </Space>
+  </Space> -->
+  <Input
+    placeholder="请输入任何信息以搜索"
+    class="search"
+    @on-blur="change_data()"
+    v-model="page.params.dname"
+  >
+    <template #suffix>
+      <Icon type="ios-search" />
+    </template>
+  </Input>
   <!-- Order主表格 -->
   <Table
     class="table"
     :columns="columns"
     :data="state.info.data"
     >
+    <template #operation1="{ row, index }">
+          <div class="operation1"> 
+            <Button type="info" @click="openModal2(row.id,row.status)">修改状态</Button>
+            <Modal
+                v-model="page.params.modal2"
+                title="修改状态"
+                @on-ok="changeStatus()"
+                @on-cancel="page.params.modal2 = false">
+                <RadioGroup v-model="state.info.order_status" vertical>
+                    <Radio label="1" :disabled='page.params.statusDisabled'>体检中</Radio>
+                    <Radio label="2" >增检中</Radio>
+                    <Radio label="3" >已完成</Radio>
+                </RadioGroup>
+            </Modal>
+          </div>
+    </template>
     <template #operation="{ row, index }">
           <div class="operation">    
             <!-- modal:  -->
-            <Button type="info" @click="openModal(row.id)">增加检查</Button>
+            <Button type="info" @click="openModal(row.id,row.status)">增加检查</Button>
             <Modal
-                v-model="state.info.modal"
+                v-model="page.params.modal"
                 title="增加检查"
                 @on-ok="addCategory()"
-                @on-cancel="state.info.modal = false">
+                @on-cancel="page.params.modal = false">
                 <b>选择需要增加的检查项目</b><br>
                 <RadioGroup v-model="state.info.name" vertical>
-                    <Radio label="一般检查" :disabled='state.info.disabled[1]'>一般检查</Radio>
-                    <Radio label="内科检查" :disabled='state.info.disabled[2]'>内科检查</Radio>
-                    <Radio label="外科检查" :disabled='state.info.disabled[3]'>外科检查</Radio>
-                    <Radio label="心电图" :disabled='state.info.disabled[4]'>心电图</Radio>
-                    <Radio label="耳鼻喉检查" :disabled='state.info.disabled[5]'>耳鼻喉检查</Radio>
-                    <Radio label="肝胆脾胰彩超" :disabled='state.info.disabled[6]'>肝胆脾胰彩超</Radio>
-                    <Radio label="眼科检查" :disabled='state.info.disabled[7]'>眼科检查</Radio>
-                    <Radio label="甲状腺超声" :disabled='state.info.disabled[8]'>甲状腺超声</Radio>
-                    <Radio label="血常规" :disabled='state.info.disabled[9]'>血常规</Radio>
-                    <Radio label="尿常规" :disabled='state.info.disabled[10]'>尿常规</Radio>
-                    <Radio label="肾功能" :disabled='state.info.disabled[11]'>肾功能</Radio>
-                    <Radio label="糖化血红蛋白" :disabled='state.info.disabled[12]'>糖化血红蛋白</Radio>
-                    <Radio label="空腹血糖" :disabled='state.info.disabled[13]'>空腹血糖</Radio>
-                    <Radio label="肿瘤12项" :disabled='state.info.disabled[14]'>肿瘤12项</Radio>
+                    <Radio label="一般检查" :disabled='page.params.disabled[1]'>一般检查</Radio>
+                    <Radio label="内科检查" :disabled='page.params.disabled[2]'>内科检查</Radio>
+                    <Radio label="外科检查" :disabled='page.params.disabled[3]'>外科检查</Radio>
+                    <Radio label="心电图" :disabled='page.params.disabled[4]'>心电图</Radio>
+                    <Radio label="耳鼻喉检查" :disabled='page.params.disabled[5]'>耳鼻喉检查</Radio>
+                    <Radio label="肝胆脾胰彩超" :disabled='page.params.disabled[6]'>肝胆脾胰彩超</Radio>
+                    <Radio label="眼科检查" :disabled='page.params.disabled[7]'>眼科检查</Radio>
+                    <Radio label="甲状腺超声" :disabled='page.params.disabled[8]'>甲状腺超声</Radio>
+                    <Radio label="血常规" :disabled='page.params.disabled[9]'>血常规</Radio>
+                    <Radio label="尿常规" :disabled='page.params.disabled[10]'>尿常规</Radio>
+                    <Radio label="肾功能" :disabled='page.params.disabled[11]'>肾功能</Radio>
+                    <Radio label="糖化血红蛋白" :disabled='page.params.disabled[12]'>糖化血红蛋白</Radio>
+                    <Radio label="空腹血糖" :disabled='page.params.disabled[13]'>空腹血糖</Radio>
+                    <Radio label="肿瘤12项" :disabled='page.params.disabled[14]'>肿瘤12项</Radio>
                 </RadioGroup>
             </Modal>
           </div>
@@ -206,5 +269,9 @@ async function addCategory() {
   button {
     margin-right: 20px;
   }
+}
+.search {
+  width: auto;
+  margin: 20px 20px 0 20px;
 }
 </style>
